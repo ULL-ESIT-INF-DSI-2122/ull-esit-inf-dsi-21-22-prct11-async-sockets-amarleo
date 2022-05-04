812bb475fc5bda8d718529666bc1934f92389e82
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-05-04 18:25:46 +0100

    server update

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
-export function index(): string {
-  return 'index';
-}
+import * as net from 'net';
+// import EventEmitter from 'events';
+
+net.createServer((connection) => {
+  console.log('A client has connected.');
+
+  connection.write(`Connection established.`);
+
+  connection.on('close', () => {
+    console.log('A client has disconnected.');
+  });
+}).listen(60300, () => {
+  console.log('Waiting for clients to connect.');
+});
